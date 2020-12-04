43a1a3d8a4b9fc0b618c2a3ae885b8a627489360
nblitte <nblitte@bitoo.fr>
2020-12-04 17:49:24 +0100

    Mon projet Java

---
+++ src/main/java/Initiation.java
@@ -0,0 +1,10 @@
+public class Initiation {
+    boolean choix = true;
+    byte nbcourt = 30;
+    int entier = 45800;
+    float pi = 3.1416f;
+    char caractere = 'a';
+    String mot = new String("Bonjour Nathalie");
+
+    int nb =1; nb=2; nb=3;
+}
